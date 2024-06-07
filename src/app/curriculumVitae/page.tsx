acc1784d4f70155622f9cfc0b1d97dd59be8686b
Gabriel <gfsgetizo@gmail.com>
2024-06-07 23:11:15 +0800

    final version 3

--- src/app/curriculumVitae/page.tsx
+++ src/app/curriculumVitae/page.tsx
@@ -22,13 +22,31 @@ export default function Page(){
     >
       <CardContent className="p-4 flex flex-row justify-between space-x-4">
         <div className="flex-shrink-0">
-          <p className="text-base font-bold">2024-Present</p>
+          <p className="text-base font-bold">2024-2025</p>
           <p className="lg:text-base text-sm font-medium">ALPHA Front-End Developer</p>
           <p className="lg:text-base text-sm">STI Ortigas-Cainta</p>
         </div>
         <div className="flex-grow">
-            <p className="lg:text-base text-sm">I am currently a Front-End Developer in the
+            <p className="lg:text-base text-sm">I am a Front-End Developer in the
             A.L.P.H.A: Alliance of Leading Programmers through Heuristic Approach
+            </p>
+        </div>
+      </CardContent>
+    </Card>
+  </div>
+  <div>
+    <Card className="lg:w-[37.5rem] w-[20rem] h-auto rounded"
+    style={{boxShadow:'20px 20px 45px #d4d4d4, -20px -20px 45px #ffffff'}}
+    >
+      <CardContent className="p-4 flex flex-row justify-between space-x-4">
+        <div className="flex-shrink-0">
+          <p className="text-base font-bold">2027-Present</p>
+          <p className="lg:text-base text-sm font-medium">Full-Stack Web Developer</p>
+          <p className="lg:text-base text-sm">Freelancing</p>
+        </div>
+        <div className="flex-grow">
+            <p className="lg:text-base text-sm">I freelance my skills as a Full-Stack Web Developer. You can contact me through
+              Facebook and Gmail about my rates!
             </p>
         </div>
       </CardContent>
@@ -48,6 +66,24 @@ export default function Page(){
         </div>
         <div className="flex-grow">
           <p className="lg:text-base text-sm">I am knowledgeable of OOP programming langugaes like C++ and Java. I also know vanilla HTML, CSS, Js, and Ts</p>
+        </div>
+      </CardContent>
+    </Card>
+  </div>
+  <h1 className="relative flex justify-center mb-10 font-bold">Hobbies</h1>
+  <div>
+    <Card className="lg:w-[37.5rem] w-[20rem] h-auto rounded"
+    style={{boxShadow:'20px 20px 45px #d4d4d4, -20px -20px 45px #ffffff'}}
+    >
+      <CardContent className="p-4 flex flex-row justify-between space-x-4">
+        <div className="flex-shrink-0">
+          <p className="text-base font-bold">Hobbies:</p>
+          <p className="lg:text-base text-sm">Singing</p>
+          <p className="lg:text-base text-sm">Playing Video Games</p>
+          <p className="lg:text-base text-sm">Reading about History</p>
+        </div>
+        <div className="flex-grow">
+          <p className="lg:text-base text-sm">My hobbies are singing, playing single player video games, and reading or watching about history. </p>
         </div>
       </CardContent>
     </Card>
